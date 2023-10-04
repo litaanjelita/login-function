87ee0fe42bb0c29227d3c25ecaf35e195dd1d3f6
lita anjelita <141709031+litaanjelita@users.noreply.github.com>
2023-10-04 14:50:09 +0700

    Add files via upload

---
+++ login_reg_function.js
@@ -0,0 +1,12 @@
+function login  (username,pass) {
+    console.log("--------------------")
+    console.log("login")
+    console.log("--------------------")
+    console.log("username:"+username)
+    console.log("--------------------")
+    console.log("password:"+pass)
+}
+
+login("litaa","************")
+
+
